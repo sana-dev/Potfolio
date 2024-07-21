77fce39a2ac93e2041af52bb7fbc59e2bfdd1e35
sana-dev <55602404+sana-dev@users.noreply.github.com>
2024-07-21 12:38:38 +0200

    new

--- portfolio.js
+++ portfolio.js
@@ -7,20 +7,5 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
-function addEntry() {
-
-    var email = document.getElementById('email').value;
-    var name = document.getElementById('name').value;
-    var message = document.getElementById('message').value;
 
 
-    var newRow = document.createElement('tr');
-    newRow.innerHTML = `<td>${email}</td><td>${name}</td><td>${message}</td>`;
-
-    var tableBody = document.getElementById('entriesBody');
-    tableBody.appendChild(newRow);
-
-    document.getElementById('email').value = '';
-    document.getElementById('name').value = '';
-    document.getElementById('message').value = '';
-}
